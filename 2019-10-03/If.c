f3698af658f7254ccef900d3afe759e9c5ef1f07
Milkshake-lyf <2087886585@qq.com>
2019-10-05 10:00:35 +0800

    2019/10/05 一个简单显示能整除一个数的约数(修改)

--- 2019-10-03/If.c
+++ 2019-10-03/If.c
@@ -1,18 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 int main(void){
-	int div,num;
+	unsigned long num; //要检查的数
+	unsigned long div; //可能的素数
+	bool isPrime; //素数标志
 	printf("Please enter a number:");
-	scanf("%d", &num);
-	for(div = 2; (div * div) <= num ; div++ ){
-		if( num % div == 0 ){
-			if( (div * div) != num){
-				printf("%d is divisible by %d and %d . \n", num, div, num/div);
-			}else{
-				printf("%d is divisible by %d . \n", num, div);
+	printf("Enter q to quit.\n");
+	while( scanf("%lu", &num) == 1 ){
+		for( div = 2, isPrime = true; (div * div) <= num; div++ ){
+			if( num % div == 0){
+				if( ( div * div ) != num ){
+					printf ("%lu is divisible by %lu and %lu.\n", num, div, num/div);
+				}else{
+					printf("%lu is divisible by %lu.\n", num, div);
+				}
+				isPrime = false; //不是一个素数
 			}
-			
 		}
+		if(isPrime){
+			printf("%lu is prime\n", num);
+		}
+		printf("Please enter a number:");
+		printf("Enter q to quit.\n");
 	}
 	return 0;
 }
