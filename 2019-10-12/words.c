878f328dc6a943b58a9d19e0ac3510be6eeab3c7
Milkshake-lyf <2087886585@qq.com>
2019-10-12 16:31:40 +0800

    2019/10/12 一个统计输入字符空格回车的程序

---
+++ 2019-10-12/words.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<ctype.h>
+#define STOP '#'
+int main(void){
+	//printf("Hello World!");
+	int word,space,cr;
+	word = 0; //没有初始化一直显示错误的数值
+	char ch;
+	ch = getchar();
+	//scanf("%c", &ch);
+	while( ch != STOP){
+		if(ch == '\n'){
+			cr++;
+		}else if( ch == ' ' ){
+			space++;
+		}else{
+			word++;
+		}
+		ch = getchar();
+		//scanf("%c", &ch);
+	}
+	printf("word=%d\tspace=%d\tcr=%d\n", word, space, cr);
+	return 0;
+}
+//C Primer Plus上一个统计输入字符和空格回车的代码
+//2019-10-12
