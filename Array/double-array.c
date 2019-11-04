6f84f22173703d36c7039e0dee7d3f0d2f2a0c01
Milkshake-lyf <2087886585@qq.com>
2019-11-04 20:40:22 +0800

    2019/11/04 一个简单二维数组的举例

---
+++ Array/double-array.c
@@ -0,0 +1,23 @@
+/* 一个二维数组的简单使用 */
+#include<stdio.h>
+int main( void )
+{
+        int tarray[ 3 ][ 3 ] = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 },
+        };
+        /* 定义并初始化一个二维数组 */
+        int index1, index2;
+        /* 打印出二维数组中各个元素的值 */
+        for ( index1 = 0; index1 < 3; index1 ++ )
+        {
+                for ( index2 = 0; index2 < 3; index2 ++ )
+                {
+                        printf("第[%d][%d]个元素是%2d\t", index1, index2, tarray[ index1 ][ index2 ] );
+                }
+                putchar( '\n' ); //打印完一行后换行
+                putchar( '\n' ); //打印完一行后换行
+        }
+        return 0;
+}
