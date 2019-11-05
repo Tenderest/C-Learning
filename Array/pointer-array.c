c6992d7b4c5e84314a95a895b67939fe5b4cfcb8
Milkshake-lyf <2087886585@qq.com>
2019-11-05 08:59:19 +0800

    2019/11/05 一个数组地址和指针的关系的举例 C Primer Plus page255

---
+++ Array/pointer-array.c
@@ -0,0 +1,36 @@
+/* 一个数组的地址的例子 */
+#include<stdio.h>
+int main( void )
+{
+        int array[ 3 ];
+        array[ 0 ] = 1;
+        array[ 1 ] = 2;
+        array[ 2 ] = 3;
+        int * parray;
+        parray = &array[ 0 ];
+        int temp;
+        temp = sizeof ( int );
+        printf("sizeof ( int ) = %d\n",temp);
+        int index;
+        for ( index = 0; index < 3; index ++ )
+        {
+                printf("pointer + %d : array[ %d ] = %d  %p\n",index, index, array[ index ], parray + index );
+        }
+        return 0;
+}
+/*
+        double array[ 3 ];
+        array[ 0 ] = 1.0;
+        array[ 1 ] = 2.0;
+        array[ 2 ] = 3.0;
+        double * parray;
+        parray = &array[ 0 ];
+        int temp;
+        temp = sizeof ( double );
+        printf("sizeof ( double ) = %d\n",temp);
+        int index;
+        for ( index = 0; index < 3; index ++ )
+        {
+                printf("pointer + %d : array[ %d ] = %f  %p\n",index, index, array[ index ], parray + index );
+        }
+*/
