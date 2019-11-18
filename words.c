d0ebe91a5f3c69c9771be08f42557c8fe6b41cf1
Tenderest <2087886585@qq.com>
2019-11-18 16:59:09 +0800

    2019/11/18 整理了一下文件

--- words.c
+++ words.c
@@ -1,3 +1,5 @@
+//C Primer Plus上一个统计输入字符和空格回车的代码
+//2019-10-12
 #include<stdio.h>
 #include<ctype.h>
 #define STOP '#'
@@ -22,5 +24,3 @@ int main(void){
 	printf("word=%d\tspace=%d\tcr=%d\n", word, space, cr);
 	return 0;
 }
-//C Primer Plus上一个统计输入字符和空格回车的代码
-//2019-10-12
