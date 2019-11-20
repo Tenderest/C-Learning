0521c5591f6b22ff63e1ff7724da4b3a1a4a3ad5
Tenderest <2087886585@qq.com>
2019-11-20 08:58:44 +0800

    Class 3x4

---
+++ Class/3x4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main( void )
+{
+        int ar[3][4];
+        int i, j, max = 0;
+        int tempi, tempj;
+        for ( i = 0; i < 3; i ++ )
+        {
+                for ( j =0; j < 4; j++ )
+                {
+                        printf("请输入第%d行第%d个数：", i + 1, j + 1);
+                        scanf("%d",&ar[ i ][ j ]);
+                }
+        }
+        for (i=0; i<3; i++ )
+        {
+                for (j=0; j<4; j++ )
+                {
+                        if (max<ar[i][j])
+                        {
+                                max = ar[i][j];
+                                tempi = i;
+                                tempj = j;
+                        }
+                }
+        }
+        printf("最大的数是%d\n它的数组下标是[%d, %d]\n",max, tempi, tempj);
+        return 0;
+}
