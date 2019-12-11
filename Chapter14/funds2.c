1410214716386e9215322d881ee6c2a29c70832c
Tenderest <2087886585@qq.com>
2019-12-11 19:50:31 +0800

    2019/12/11 函数-结构 使用结构的指针，传递指向结构的指针

---
+++ Chapter14/funds2.c
@@ -0,0 +1,26 @@
+/* 使用结构的地址 */
+/* 传递指向结构的指针 */
+#include<stdio.h>
+#define FUNDLEN 50
+struct funds {
+        char bank[FUNDLEN];
+        double bankfund;
+        char save[FUNDLEN];
+        double savefund;
+};
+double sum ( const struct funds * ); // 参数是一个指针
+int main(void)
+{
+        struct funds stan = {
+                "Garlic-Melon Bank",
+                3024.72,
+                "Lucky's Saving and Lan",
+                9237.11
+        };
+        printf("Stan has a total of $%.2f.\n", sum( &stan ) );
+        return 0;
+}
+double sum ( const struct funds * money)
+{
+        return ( money->bankfund + money->savefund );
+}
