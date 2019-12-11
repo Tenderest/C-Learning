c88576ae096f4616e8ecac82847f20acab03b1fe
Tenderest <2087886585@qq.com>
2019-12-11 19:50:58 +0800

    2019/12/11 函数-结构 把结构作为参数传递给函数

---
+++ Chapter14/funds3.c
@@ -0,0 +1,25 @@
+/* 把结构作为参数传递 */
+#include<stdio.h>
+#define FUNDLEN 50
+struct funds {
+        char bank[FUNDLEN];
+        double bankfund;
+        char save[FUNDLEN];
+        double savefund;
+};
+double sum ( struct funds moolah ); // 参数是一个结构
+int main(void)
+{
+        struct funds stan = {
+                "Garlic-Melon Bank",
+                3024.72,
+                "Lucky's Saving and Lan",
+                9237.11
+        };
+        printf("Stan has a total of $%.2f.\n", sum( stan ) );
+        return 0;
+}
+double sum ( struct funds moolah )
+{
+        return ( moolah.bankfund + moolah.savefund );
+}
