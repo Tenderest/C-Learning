c43104cc75f31c3058833b14f3a3c7e51badce69
Tenderest <2087886585@qq.com>
2019-12-12 09:10:06 +0800

    2019/12/12 函数-结构 传递给函数一个结构和函数返回一个结构

---
+++ Chapter14/names2.c
@@ -0,0 +1,41 @@
+/* 传递和返回结构 */
+#include<stdio.h>
+#include<string.h>
+
+struct namect {
+        char fname[20];
+        char lname[20];
+        int letters;
+};
+
+struct namect getinfo ( void ); // 返回值是一个结构
+struct namect makeinfo ( struct namect ); // 参量是一个结构 省略的了参量的名字
+void showinfo ( struct namect );
+
+int main(void)
+{
+        struct namect person;
+        person = getinfo();
+        person = makeinfo(person);
+        showinfo(person);
+        return 0;
+}
+struct namect getinfo ( void )
+{
+        struct namect temp;
+        printf("Please enter your first name:");
+        gets(temp.fname);
+        printf("Please enter you last name:");
+        gets(temp.lname);
+        return temp;
+        
+}
+struct namect makeinfo ( struct namect info )
+{
+        info.letters = strlen(info.fname) + strlen(info.lname);
+        return info;
+}
+void showinfo ( const struct namect infoo)
+{
+        printf("%s %s, your name contains %d letters.\n", infoo.fname, infoo.lname, infoo.letters);
+}
