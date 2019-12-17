d62f4285ea08f2e3c5735a604eda8b604c2fe679
Tenderest <2087886585@qq.com>
2019-12-17 20:42:03 +0800

    :tada: 使用枚举类型

--- Chapter14/enum.c
+++ Chapter14/enum.c
@@ -1,4 +1,10 @@
 /* 使用枚举值 */
+/* 使用枚举默认时的值被按顺序指定为0、1、2 */
+/* enum kids { nippy, slats, skippy, nina, liz}; */
+/* 指定值：需要在声明中包含期望值 */
+/* enum levels { low = 100, medium = 500, high = 2000 }; */
+/* 如果只对一个常量赋值，而没有对后面的常量赋值，那么后面的常量会被赋予后续的值 */
+/* enum feline { cat, lynx = 10, puma, tiger }; */
 #include<stdio.h>
 #include<string.h>
 #include<stdbool.h>
