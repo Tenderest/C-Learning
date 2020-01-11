2a315482491763decc181c24bb4b52f57a6e8dd4
Tenderest <2087886585@qq.com>
2020-01-11 11:44:25 +0800

    2020/01/11 :tada: C Primer Plus Chapter3 Programming Exercises-T3

---
+++ Programming Exercises/Chapter3/Chapter3-T3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+    // \a ASCII = '\007' 警报(ANSI C)
+    printf("\aStartled by the sudden sound, Sally shouted, \"By the Great Pumpkin, What was that!\"\n");
+    return 0;
+}
